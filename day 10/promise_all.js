e0bc4db685b80d023ffd726dbf561ad9e2ad8b69
pitamchandra <pitamchandra402@gmail.com>
2025-03-10 13:55:11 +0600

    promise all

---
+++ day 10/promise_all.js
@@ -0,0 +1,10 @@
+const p1 = new Promise((resolve, reject) => resolve("operation successful"))
+const p2 = new Promise((resolve, reject) => resolve("operation successful"))
+const p3 = new Promise((resolve, reject) => reject("operation fail"))
+
+Promise.all([p1,p2,p3])
+    .then(result => console.log(result))
+    .catch((err) => console.log(err))
+    .finally(()=> "done")
+
+// if any result come fail, all will print fail result
