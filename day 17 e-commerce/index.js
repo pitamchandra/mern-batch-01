8d631b18e535fd3dc5c0e0467d4a6064d8689b2d
Palash-Chandra-Roy <palashtpi21@gmail.com>
2025-04-12 13:31:37 +0600

    product put patch delete and localhost to globalhost

--- day 17 e-commerce/index.js
+++ day 17 e-commerce/index.js
@@ -4,8 +4,10 @@ const categoriesRoute = require('./routes/categories')
 const productsRoute = require('./routes/products')
 const customerRoute = require('./routes/customers')
 require('dotenv').config()
-const db = require("./db")
+// const db = require("./db")
 const port = process.env.SERVER_PORT || 3000;
+const host = process.env.SERVER_IP || "127.0.0.1";
+// console.log(host);
 
 const app = express()
 
@@ -16,13 +18,12 @@ app.get('/', (req, res) => {
     res.send("server is running......")
 })
 
-
 app.use('/categories', categoriesRoute)
 
 app.use('/products', productsRoute)
 
 app.use('/customers', customerRoute)
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+app.listen(port, host, () => {
+    console.log(`server is running on port ${host}:${port}`);
 })
--- day 17 e-commerce/routes/products.js
+++ day 17 e-commerce/routes/products.js
@@ -34,5 +34,85 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    const sql = 'SELECT * FROM products WHERE id = ?'
+    db.query(sql, [id], (err, result) => {
+        if(err){
+            res.status(500).json({
+                status: "error",
+                message: err.message
+            })
+        }else if(result.length === 0){
+            res.status(404).json({
+                status: "failed",
+                message: "Product not found"
+            })
+        }
+        else{
+            res.status(200).json({
+                status: "success",
+                message: "getting product by id successfully",
+                data: result
+            })
+        }
+    })
+})
+
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const {name, price, description} = req.body
+    const sql = 'UPDATE products SET name = ?, price = ?, description = ? WHERE id = ?';
+    db.query(sql, [name, price, description, id], (err, result) => {
+        if(err){
+            res.status(500).json({
+                status: "error",
+                message: err.message
+            })
+        }else if(result.affectedRows === 0){
+            res.status(404).json({
+                status: "failed",
+                message: "Product not found"
+            })
+        }
+        else{
+            res.status(200).json({
+                status: "success",
+                message: "product updated successfully",
+                data: {id, name, price, description}
+            })
+        }
+    })
+})
+router.patch('/:id', (req, res) => {
+    const {id} = req.params
+    const data = req.body
+    const fields = Object.keys(data)
+    const values = Object.values(data)
+    const setClause = fields.map(field => `${field} = ?`).join(', ')
+    // console.log(...values);
+    const sql = `UPDATE products SET ${setClause} WHERE id = ?`
+    db.query(sql, [...values, id], (err, result) => {
+        // console.log(result);
+        if(err){
+            res.status(500).json({
+                status: "error", 
+                message: err.message
+            })
+        }else if(result.affectedRows === 0){
+            res.status(404).json({
+                status: "failed", 
+                message: "product not found"
+            })
+        }else{
+            res.status(200).json({
+                status: "success", 
+                message: "product updated successfully",
+                data: {id, ...data}
+            })
+        }
+    })
+
+})
 
 module.exports = router;
